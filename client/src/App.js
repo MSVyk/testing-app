f7ff5f05534f1b2d33a8779cee95557148204fe3
Yuriy Kuptsov <ykuptsov@csinow.edu>
2024-07-15 17:50:55 -0400

    adding redux

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import store from './store';
-// import Navbar from './components/Navbar';
-// import Alert from './components/Alert';
-// import NotFound from './components/NotFound';
 import HomePage from './homePage';
 import ProfilePage from './profile';
 
@@ -12,18 +9,9 @@ function App() {
   return (
     <Provider store={store}>
       <Router>
-        {/* <Navbar />
-        <Alert /> */}
         <Routes>
           <Route path="/" element={<HomePage />} />
-          {/* <Route path="register" element={<Register />} />
-          <Route path="login" element={<Login />} /> */}
           <Route path="profile/" element={<ProfilePage />} />
-          {/* <Route
-            path="create-profile"
-            element={<PrivateRoute component={ProfileForm} />}
-          /> */}
-          {/* <Route path="/*" element={<NotFound />} /> */}
         </Routes>
       </Router>
     </Provider>
